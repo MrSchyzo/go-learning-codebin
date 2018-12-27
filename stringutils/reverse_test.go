feb2b69023a8aac92c72a6da168cbbb07d13dcb0
Mr.Schyzo <catapano-marco@mail.com>
2018-12-27 12:34:57 +0100

    Adds comments to take some notes about testing

--- stringutils/reverse_test.go
+++ stringutils/reverse_test.go
@@ -2,8 +2,16 @@ package stringutils
 
 import "testing"
 
+/*
+Tests - 101.
+1- Create a file with <name>_test.go
+2- Package may be the same of the tested functions, remember to import "testing" package
+3- Write a void function named Test<whatToTest> with argument of type *testing.T
+4- After tests are written, "cd" on the same directory of *_test.go and execute "go test"
+*/
 func TestReverseString(t *testing.T) {
 
+	// Creates a struct on the fly for acceptance test
 	cases := []struct {
 		given string
 		then  string
@@ -15,10 +23,12 @@ func TestReverseString(t *testing.T) {
 		{"öl lë", "ël lö"},
 	}
 
+	//Remember, for in go must enumerate key and value, that's why you have and underscore at first
 	for _, c := range cases {
 
 		then := ReverseString(c.given)
 
+		//Is there any equivalent of string templating? My guess is "no" seeing the "simplicity" of Go
 		if then != c.then {
 			t.Errorf("ReverseString(%q) returned %q, expected %q", c.given, then, c.then)
 		}
